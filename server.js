865f14865a8d8be8964eed6f8a2734be535048ca
WebCreator <72634873+Freework-site@users.noreply.github.com>
2024-02-29 04:47:15 -0700

    Cors_JSon

--- server.js
+++ server.js
@@ -12,12 +12,19 @@ app.use(cors()); // Add this line to enable CORS
 mongoose.connect('mongodb+srv://miarslan555:PswxfNRcQ7y0wBnG@snapx.cseqful.mongodb.net/?retryWrites=true&w=majority&appName=SnapX/extension_users')
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Failed to connect to MongoDB:', err));
-    
+
 // Define a schema for the data
 const userDataSchema = new mongoose.Schema({
     email: String,
     password: String
 });
+
+// Create a Mongoose model based on the schema
+const UserData = mongoose.model('UserData', userDataSchema);
+
+app.use(express.json());
+app.use(cors());
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
