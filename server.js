410de9ffe5bce7a846719f089c067bc129f55db3
WebCreator <72634873+Freework-site@users.noreply.github.com>
2024-02-29 04:44:23 -0700

    mongodb

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -7,10 +8,10 @@ app.use(express.json());
 app.use(cors()); // Add this line to enable CORS
 
 // Endpoint to receive data from the frontend
-
-app.get('/', (req,res) => {
-    res.send("Hello buddy")
-})
+//Mangodb Connection
+mongoose.connect('mongodb+srv://miarslan555:PswxfNRcQ7y0wBnG@snapx.cseqful.mongodb.net/?retryWrites=true&w=majority&appName=SnapX/extension_users')
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Failed to connect to MongoDB:', err));
 
 // Start the server
 app.listen(PORT, () => {
